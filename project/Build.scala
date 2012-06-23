0e2272ba124dd7455be67c8be95f4f90bf4c665d
Leon Radley <leon@radley.se>
2012-06-23 08:51:02 +0200

    First release

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 
 object ProjectBuild extends Build {
 
-  lazy val buildVersion =  "1.0.0-SNAPSHOT"
+  lazy val buildVersion =  "1.0.0"
 
   lazy val root = Project(id = "play-plugins-enumeration", base = file("."), settings = Project.defaultSettings).settings(
     organization := "se.radley",
