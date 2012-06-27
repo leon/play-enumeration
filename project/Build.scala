9d63fe3b47d0e40e0280bbdb3cfacb00c2a88c03
Leon Radley <leon@digiplant.se>
2012-06-27 09:11:47 +0200

    v1.0.1 Built against Play 2.0.2

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 
 object ProjectBuild extends Build {
 
-  lazy val buildVersion =  "1.0.0"
+  lazy val buildVersion =  "1.0.1"
 
   lazy val root = Project(id = "play-plugins-enumeration", base = file("."), settings = Project.defaultSettings).settings(
     organization := "se.radley",
@@ -12,8 +12,8 @@ object ProjectBuild extends Build {
     scalaVersion := "2.9.1",
     resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/",
     resolvers += "Typesafe Snapshots" at "http://repo.typesafe.com/typesafe/snapshots/",
-    libraryDependencies += "play" %% "play" % "2.0.1",
-    libraryDependencies += "play" %% "play-test" % "2.0.1" % "test",
+    libraryDependencies += "play" %% "play" % "2.0.2",
+    libraryDependencies += "play" %% "play-test" % "2.0.2" % "test",
 
     publishMavenStyle := true,
     publishArtifact in Test := false,
